6c7cf0a73ca015745b1069d7508bbbae62526de2
MansAlien <mans012033945421@gmail.com>
2024-01-29 15:22:38 +0200

    (create) urls and views, and all the templates

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import UpdateView, DeleteView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 from .models import Article
 
@@ -14,9 +14,14 @@ class ArticleDetailView(DetailView):
 class ArticleUpdateView(UpdateView):
     model = Article
     fields = ('title', 'body')
-    template_name = "article_update.html"
+    template_name = "article_edit.html"
 
 class ArticleDeleteView(DeleteView):
     model = Article
     template_name = "article_delete.html"
     success_url = reverse_lazy("article_list")
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = "article_new.html"
+    fields = ('title', 'body', "author")
